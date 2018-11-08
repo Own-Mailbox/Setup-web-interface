53540436dd57d65e8253b746281e6d8eeaac20b6
Pierre Parent <pierre.parent@pparent.fr>
2018-11-08 23:25:09 +0100

    One more update.

--- scripts/setup-tor.sh
+++ scripts/setup-tor.sh
@@ -9,8 +9,7 @@ HiddenServicePort 25 127.0.0.1:25\n"
 
 printf "$text" >> /etc/tor/torrc;
 
-pid=$(cat /tmp/tor-hidden-service-pid)
-kill -9 $pid;
+killall tor
 
 #Read 32 Bytes of entropy just to make sure /dev/urandom is correctly reseeded
 #See https://lists.torproject.org/pipermail/tor-talk/2014-January/031773.html
@@ -20,5 +19,6 @@ touch /var/log/tor.log
 chown tor /var/log/tor.log
 sleep 2;
 su tor -c "tor&"
+su tor -c "tor -f /etc/tor/torrc-consult &"
 
 touch /etc/omb/admin-pass-configured
